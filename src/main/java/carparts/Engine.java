7f00efec0f7d644b870224841ad8499e9ec4cf08
SergiyBoiko <codedva00@gmail.com>
2017-02-04 10:08:41 +0200

    add classes

---
+++ src/main/java/carparts/Engine.java
@@ -0,0 +1,18 @@
+package carparts;
+
+import java.math.BigDecimal;
+
+public class Engine {
+
+    private BigDecimal engineСapacity;
+
+    public BigDecimal getEngineСapacity() {
+        return engineСapacity;
+    }
+
+    public void setEngineСapacity(BigDecimal engineСapacity) {
+        this.engineСapacity = engineСapacity;
+    }
+
+
+}
